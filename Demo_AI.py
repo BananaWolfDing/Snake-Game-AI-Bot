e9b952463051fa26f901eec83fa6bc6e2aaadbcb
Zhaobo <zbding@rsvp.ai>
2018-12-17 11:29:56 -0500

    finished the demo part

--- Demo_AI.py
+++ Demo_AI.py
@@ -1,5 +1,25 @@
+import pygame
+import sys
+from configs import configs
+from modules.Game import Game
 from train.Experience import ERM
-
+from train.NeuralNetwork import NeuralNetwork
 erm = ERM()
 erm.loadData()
 
+pygame.init()
+screen = pygame.display.set_mode((configs.WIN_WIDTH, configs.WIN_HEIGHT))
+pygame.display.set_caption("Snake Game AI Bot")
+
+game = Game(screen)
+nn = NeuralNetwork()
+nn.loadModel()
+
+dirc = 0
+while True:
+    observe, reward, len, lose = game.moveByRelativeDirection(dirc)
+    if not lose:
+        dirc = nn.predict(observe)
+
+    pygame.display.update()
+    pygame.time.Clock().tick(configs.GAME_FPS)
