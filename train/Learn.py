1b875763f6a14fee8ad1d75b14be9e91ac528bb4
Zhaobo Ding <me@dingzhaobo.net>
2018-12-19 01:53:27 -0500

    added test for learning module

--- train/Learn.py
+++ train/Learn.py
@@ -1,5 +1,21 @@
+import pygame
+from modules.Game import Game
 from train.NeuralNetwork import NeuralNetwork
 from train.Experience import ERM
+
+
+def test(nn):
+    game = Game()
+
+    dirc = 0
+    score = 0
+    while True:
+        observe, reward, len, lose = game.moveByRelativeDirection(dirc)
+        if not lose:
+            dirc = nn.predict(observe)
+        else:
+            return len
+
 
 nn = NeuralNetwork()
 erm = ERM()
@@ -8,3 +24,5 @@ data = erm.loadData()
 
 nn.train(data)
 nn.saveModel()
+
+print("Score = " + str(test(nn)))
